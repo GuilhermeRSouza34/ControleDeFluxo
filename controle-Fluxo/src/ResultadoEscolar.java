1bf0e3a81a1509aeee5dfc5e1d2fb31ca559f40b
Guilherme <guilhermersouza34@gmail.com>
2024-05-02 11:40:35 -0300

    Atualizandoo A Classe ResultadoEscolar e utilizando a condição encadeada

--- controle-Fluxo/src/ResultadoEscolar.java
+++ controle-Fluxo/src/ResultadoEscolar.java
@@ -4,6 +4,8 @@ public class ResultadoEscolar {
 
         if (nota >= 7) {
             System.out.println("Aprovado");
+        } else if (nota >= 5 && nota < 7) {
+            System.out.println("Você está de Recuperação");
         } else {
             System.out.println("Reprovado");
         }
